5ad753f046b7cea13a4a279876061c330be1064a
SamuelAlvarezT <samuesteban70@gmail.com>
2024-07-16 17:10:01 -0500

    feat: :sparkles: has started the next course
    has started the next course  and make partially the first module

---
+++ logicaDeProgramcion/explorar funciones y listas/Aula1/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del númenro secreto'
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML= 'Indica un numero del 1 al 10 ';
+
+function intentoDeUsuario(){
+    alert ('click en desde el boton')
+
+};
